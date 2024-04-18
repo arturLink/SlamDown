cfafbd1126ddab8d4ad54c0391a98c072778f584
vafle <vafle@DESKTOP-D6239CC>
2024-04-18 20:18:16 +0300

    Bottom navigation panel added

---
+++ Screens/RosterPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+//import { SafeAreaView } from 'react-native-safe-area-context';
+
+const RosterPage = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.container}>
+        <Text style={styles.text}>Hello, Roster page!</Text>
+        </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default RosterPage;
